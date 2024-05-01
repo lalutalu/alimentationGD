7b9bd999ee48ebcd534e61032083332f72c73279
Lukas Alutalu <121912040+lalutalu@users.noreply.github.com>
2024-05-01 09:34:48 -0400

    finished!

--- python/newcsv.py
+++ python/newcsv.py
@@ -1,5 +1,6 @@
 import re
 import os
+import csv
 import PyPDF2
 from numpy.lib.shape_base import row_stack
 from reportlab.pdfgen import canvas
@@ -10,6 +11,7 @@ import pandas as pd
 noCode = 0
 PERCENTAGE = 14.95
 
+
 class Product:
     def __init__(self, name, prixOg, prixNew, code39, weight, unit, handleid) -> None:
         self.name = name
@@ -19,10 +21,14 @@ class Product:
         self.weight = weight
         self.unit = unit
         self.handleid = handleid
+        self.fieldType = "Product"
+        self.Visible = "true"
+        self.DiscountMode = "PERCENT"
+        self.Inventory = "InStock"
 
 
 def extract_product_name(product_line: str) -> str:
-    pattern = r'IMPACT-\d+\s+(.*?)\s+(\d{3}-\d{5}-\d{5})'
+    pattern = r"IMPACT-\d+\s+(.*?)\s+(\d{3}-\d{5}-\d{5})"
     match = re.search(pattern, product_line)
     if match:
         captured_text = match.group(1)
@@ -33,7 +39,7 @@ def extract_product_name(product_line: str) -> str:
 
 
 def extract_product_upc(product_line: str) -> str:
-    pattern = r'IMPACT-\d+\s+(.*?)\s+(\d{3}-\d{5}-\d{5})'
+    pattern = r"IMPACT-\d+\s+(.*?)\s+(\d{3}-\d{5}-\d{5})"
     match = re.search(pattern, product_line)
     if match:
         captured_text = match.group(2)
@@ -44,7 +50,7 @@ def extract_product_upc(product_line: str) -> str:
 
 
 def extract_product_code(product_line: str) -> str:
-    match = re.search(r'\b(\d{7})[A-Za-z]?\b', product_line)
+    match = re.search(r"\b(\d{7})[A-Za-z]?\b", product_line)
     if match:
         return match.group(1)
     else:
@@ -53,18 +59,18 @@ def extract_product_code(product_line: str) -> str:
 
 
 def extract_product_with_details(pdf_path: str) -> list:
-    with open(pdf_path, 'rb') as pdf:
+    with open(pdf_path, "rb") as pdf:
         reader = PyPDF2.PdfReader(pdf, strict=False)
         product_lines = []
         for page_num in range(len(reader.pages)):
             page = reader.pages[page_num]
             content = page.extract_text()
 
-            lines = content.split('\n')
+            lines = content.split("\n")
             product_line = ""
 
             for line in lines:
-                if re.match(r'^IMPACT-\d+\s+', line):
+                if re.match(r"^IMPACT-\d+\s+", line):
                     product_line = line
                 elif product_line:
                     product_line += f" {line}"
@@ -74,25 +80,25 @@ def extract_product_with_details(pdf_path: str) -> list:
 
 
 def extract_original_price(product_line: str) -> str:
-    match = re.search(r'\b(\d+\.\d+)\s+', product_line)
+    match = re.search(r"\b(\d+\.\d+)\s+", product_line)
     return match.group(1) if match else "No Original Price"
 
 
 def extract_quantity(product_line: str) -> str:
-    match = re.search(r'\d+\s+(\d+)', product_line)
+    match = re.search(r"\d+\s+(\d+)", product_line)
     return match.group(1) if match else "No Quantity"
 
 
 def extract_weight(product_line: str) -> tuple:
-    match = re.search(r'\d+\s+(\d+)\s+([A-Za-z]+)', product_line)
+    match = re.search(r"\d+\s+(\d+)\s+([A-Za-z]+)", product_line)
 
     if match:
         return match.groups()
     else:
-        weight_match = re.search(r'(\d+)\D*$', product_line)
+        weight_match = re.search(r"(\d+)\D*$", product_line)
         weight = weight_match.group(1) if weight_match else "No Weight"
 
-        unit_match = re.search(r'([^\d]+)$', product_line)
+        unit_match = re.search(r"([^\d]+)$", product_line)
         unit = unit_match.group(1).strip() if unit_match else "No Unit"
 
         if weight == "No Weight" and unit == "No Unit":
@@ -105,16 +111,21 @@ def calculate_new_price(ogPrice: float) -> float:
     new_price = ogPrice * (1 + PERCENTAGE / 100)
     return round(new_price, 2)
 
-def create_product_from_row(row):
-  name = row.get('name', None)
-  prixOg = row.get('prixOriginal', None)
-  prixNew = row.get('price', None)
-  code39 = row.get('sku', None)
-  weight = row.get('description', None)
-  unit = row.get('unit', "")
 
-  product = Product(name, prixOg, prixNew, code39, weight, unit, row.get('handleId', None))
-  return product
+def create_product_from_row(row):
+    handleid = row.get("handleId", None)
+    name = row.get("name", None)
+    prixOg = row.get("prixOriginal", None)
+    prixNew = row.get("price", None)
+    code39 = row.get("sku", None)
+    weight = row.get("description", None)
+    unit = row.get("unit", "")
+
+    product = Product(
+        name, prixOg, prixNew, code39, weight, unit, handleid 
+    )
+    return product
+
 
 def search_csv(filepath, create_products=False):
     data = pd.read_csv(filepath)
@@ -124,22 +135,135 @@ def search_csv(filepath, create_products=False):
         products.append(product)
     return products
 
+
 def create_csv(filepath, products):
-   headings = [ "handleId", "fieldType", "name", "description", "productImageUrl", "collection", "sku", "ribbon", "price", "surcharge", "visible",
-            "discountMode", "discountValue", "inventory", "weight", "cost", "productOptionName1", "productOptionType1", "productOptionDescription1", "productOptionName2",
-            "productOptionType2", "productOptionDescription2", "productOptionName3", "productOptionType3", "productOptionDescription3", "productOptionName4",
-            "productOptionType4", "productOptionDescription4", "productOptionName5", "productOptionType5", "productOptionDescription5", "productOptionName6",
-            "productOptionType6", "productOptionDescription6", "additionalInfoTitle1", "additionalInfoDescription1", "additionalInfoTitle2", "additionalInfoDescription2",
-            "additionalInfoTitle3", "additionalInfoDescription3", "additionalInfoTitle4", "additionalInfoDescription4", "additionalInfoTitle5", "additionalInfoDescription5",
-            "additionalInfoTitle6", "additionalInfoDescription6", "customTextField1", "customTextCharLimit1", "customTextMandatory1", "customTextField2", "customTextCharLimit2",
-            "customTextMandatory2", "brand" ];
-            
+    headings = [
+        "handleId",
+        "fieldType",
+        "name",
+        "description",
+        "productImageUrl",
+        "collection",
+        "sku",
+        "ribbon",
+        "price",
+        "surcharge",
+        "visible",
+        "discountMode",
+        "discountValue",
+        "inventory",
+        "weight",
+        "cost",
+        "productOptionName1",
+        "productOptionType1",
+        "productOptionDescription1",
+        "productOptionName2",
+        "productOptionType2",
+        "productOptionDescription2",
+        "productOptionName3",
+        "productOptionType3",
+        "productOptionDescription3",
+        "productOptionName4",
+        "productOptionType4",
+        "productOptionDescription4",
+        "productOptionName5",
+        "productOptionType5",
+        "productOptionDescription5",
+        "productOptionName6",
+        "productOptionType6",
+        "productOptionDescription6",
+        "additionalInfoTitle1",
+        "additionalInfoDescription1",
+        "additionalInfoTitle2",
+        "additionalInfoDescription2",
+        "additionalInfoTitle3",
+        "additionalInfoDescription3",
+        "additionalInfoTitle4",
+        "additionalInfoDescription4",
+        "additionalInfoTitle5",
+        "additionalInfoDescription5",
+        "additionalInfoTitle6",
+        "additionalInfoDescription6",
+        "customTextField1",
+        "customTextCharLimit1",
+        "customTextMandatory1",
+        "customTextField2",
+        "customTextCharLimit2",
+        "customTextMandatory2",
+        "brand",
+    ]
+
+
+    with open(filepath, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headings)
+        writer.writeheader()
+
+        for product in products:
+            writer.writerow(
+                {
+                    "handleId": product.handleid,
+                    "fieldType": product.fieldType,
+                    "name": product.name,
+                    "description": f"{product.weight}x{product.unit}",
+                    "productImageUrl": "",
+                    "collection": "metro",
+                    "sku": product.code39,
+                    "ribbon": "",
+                    "price": product.prixNew,
+                    "surcharge": "",
+                    "visible": product.Visible,
+                    "discountMode": product.DiscountMode,
+                    "discountValue": "0",
+                    "inventory": product.Inventory,
+                    "weight": "",
+                    "cost": "",
+                    "productOptionName1": "",
+                    "productOptionType1": "",
+                    "productOptionDescription1": "",
+                    "productOptionName2": "",
+                    "productOptionType2": "",
+                    "productOptionDescription2": "",
+                    "productOptionName3": "",
+                    "productOptionType3": "",
+                    "productOptionDescription3": "",
+                    "productOptionName4": "",
+                    "productOptionType4": "",
+                    "productOptionDescription4": "",
+                    "productOptionName5": "",
+                    "productOptionType5": "",
+                    "productOptionDescription5": "",
+                    "productOptionName6": "",
+                    "productOptionType6": "",
+                    "productOptionDescription6": "",
+                    "additionalInfoTitle1": "",
+                    "additionalInfoDescription1": "",
+                    "additionalInfoTitle2": "",
+                    "additionalInfoDescription2": "",
+                    "additionalInfoTitle3": "",
+                    "additionalInfoDescription3": "",
+                    "additionalInfoTitle4": "",
+                    "additionalInfoDescription4": "",
+                    "additionalInfoTitle5": "",
+                    "additionalInfoDescription5": "",
+                    "additionalInfoTitle6": "",
+                    "additionalInfoDescription6": "",
+                    "customTextField1": "",
+                    "customTextCharLimit1": "",
+                    "customTextMandatory1": "",
+                    "customTextField2": "",
+                    "customTextCharLimit2": "",
+                    "customTextMandatory2": "",
+                    "brand": "",
+                }
+            )
+
+    print(f"CSV file '{filepath}' created successfully.")
 
 
 pdf_products = []
 csv_products = []
 pdf_path = "../pdfs/04-03-2024.pdf"
-new_csv_path = "../../ProduitsAvecRabais.csv"
+new_csv_path = "../../../ProduitsAvecRabais.csv"
 csv_path = "../../../Produits.csv"
 combined_lines = extract_product_with_details(pdf_path)
 
@@ -153,27 +277,20 @@ for line in combined_lines:
         prixNew=calculate_new_price(float(extract_original_price(line))),
         code39=extract_product_code(line),
         weight=weight,
-        unit=unit
+        unit=unit,
     )
 
     pdf_products.append(product)
     csv_products = search_csv(csv_path)
-    # for product in csv_products:
-    #     print(product.name)
     for csv_product in csv_products:
         for pdf_product in pdf_products:
             if csv_product.code39 == pdf_product.code39:
-                # print("Product with code {} found in both CSV and PDF:".format(csv_product.code39))
-                # print("CSV Product: ", csv_product.__dict__)
-                # print("PDF Product: ", pdf_product.__dict__)
-                # print()
                 csv_product.name = pdf_product.name
                 csv_product.prixOg = pdf_product.prixOg
                 csv_product.prixNew = pdf_product.prixNew
                 csv_product.code39 = pdf_product.code39
                 csv_product.weight = pdf_product.weight
                 csv_product.unit = pdf_product.unit
-                csv_product.handleid = pdf_product.handleid
-                # print("Product updated!")
+                csv_product.handleid = csv_product.handleid
                 break
-      
+    create_csv(new_csv_path, csv_products)
