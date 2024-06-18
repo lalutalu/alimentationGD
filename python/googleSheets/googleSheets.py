069be9b623e64e21d5d70ab1f69112d10fb02294
Lukas Alutalu <121912040+lalutalu@users.noreply.github.com>
2024-06-18 10:09:29 -0700

    put files in proper directory

--- python/googleSheets/googleSheets.py
+++ python/googleSheets/googleSheets.py
@@ -1,5 +1,18 @@
 import gspread
+from googleapiclient.discovery import build
+from datetime import datetime
+
+FOLDER_ID = "1c72y-TMG3PZL2qqB7q1OJSHVbOT9R-kl"
 
 gc = gspread.oauth(credentials_filename="credentials.json")
 
-sh = gc.create('A new spreadsheet')
+sh = gc.create('')
+
+created_file_id = sh.id
+
+today = datetime.today().strftime('%d-%m-%Y')
+new_title = f"backup_{today}"
+destination_spreadsheet = gc.copy(created_file_id, folder_id=FOLDER_ID, title=new_title)
+
+print(f"Spreadsheet copied with title: {destination_spreadsheet.title}")
+gc.del_spreadsheet(created_file_id)
