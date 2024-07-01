eeb09834a05aab773591a78b80f53e521a55c2ac
Lukas Alutalu <121912040+lalutalu@users.noreply.github.com>
2024-07-01 00:02:23 -0400

    Merci Ali

--- dotnet/BottinToCsvForm/BottinToCsvForm/Parsing/ViandeParsing.cs
+++ dotnet/BottinToCsvForm/BottinToCsvForm/Parsing/ViandeParsing.cs
@@ -51,7 +51,8 @@ namespace BottinToCsvForm.Parsing
             List<string> categories = new List<string>();
             categories.Add("Viande");
             product.Code39 = code.PadLeft(8, '0');
-            product.Nom = ParseNames(dataString);
+            product.Nom = ParseNames(dataString).Replace("é", "e").Replace("â", "a").Replace("à", "a").
+                Replace("ô", "o").Replace("ê", "e").Replace("è", "e").Replace("û", "u").Replace("ù", "u").ToUpper();
             product.Taxes = LineSplitter.GetProductTaxes(dataString);
             product.Format = ParseUnit(dataString);
             product.Categories = categories;
@@ -67,18 +68,23 @@ namespace BottinToCsvForm.Parsing
             {
                 return match.Value;
             }
-            return "";
+            return "Pas de Code39";
         }
 
         public string ParseNames(string initialString)
         {
-            string pattern = @"(?<=^\d+\s+\d+\s+)(.*?)(?=\s+\d+K)"; // Capture between digits and \d+K
+            string pattern = @"(?<=^\d+\s+\d+\s+)(.*?)(?=\s+\d+K)";
             string pattern1 = @"^\d+\s+\d+\s+(.*?)(?=\s+\d{3,}g\s+\d+\s+\d+\.\d{2}\s+\d+\.\d{2}\s+\d+\.\d{2})";
+            string pattern2 = @"^\d+\s+\d+\s+(.*?)(?=\s+\d+(\.\d+)?(kg|g|ml)\s+\d+\s+\d+\.\d{2}\s+\d+\.\d{2}\s+\d+\.\d{2})";
+            string pattern3 = @"^(\d{8})\s+(\d{10,11})?\s*(.+)\b[a-zA-z]{0,12}(?=((\d+x)?\d+(g|mg|kg|ml|l|k)))";
+
             Match match = Regex.Match(initialString, pattern);
             Match match1 = Regex.Match(initialString, pattern1);
+            Match match2 = Regex.Match(initialString, pattern2);
+            Match match3 = Regex.Match(initialString, pattern3, RegexOptions.IgnoreCase);
             if (match.Success)
             {
-                return match.Groups[1].Value.Trim(); // Capture group 1 and trim whitespace
+                return match.Groups[1].Value.Trim();
             }
             else
             {
@@ -86,8 +92,16 @@ namespace BottinToCsvForm.Parsing
                 {
                     return match1.Groups[1].Value.Trim();
                 }
+                if (match2.Success)
+                {
+                    return match2.Groups[1].Value.Trim();
+                }
+                if (match3.Success)
+                {
+                    return match3.Groups[3].Value.Trim();
+                }
             }
-            return "";
+            return "No Name";
         }
 
         public string ParseUnit(string initialString)
@@ -95,13 +109,16 @@ namespace BottinToCsvForm.Parsing
             string[] patterns = {
                 @"\d{7}\s.*?(?=\s\d{1,2}\s\d+)\s\d+\s((\d+x)*\d+(\.\d+)?)\s*(G|g|l|L|KG|K|UN|U|ML|M|PQ)\b",
                 @"((\d+X)*\d+(\.\d+)?)\s*(G|g|l|L|KG|K|UN|U|ML|M|PQ)\b",
-            };
+                @"^(\d{8})\s+(\d{10,11})?\s*(.+)\b[a-zA-z]{0,12}(?=((\d+x)?\d+(g|mg|kg|ml|l|k)))"
+        };
+
 
             foreach (string pattern in patterns)
             {
                 Match match = Regex.Match(initialString, pattern);
                 if (match.Success)
                 {
+
                     string unit = match.Groups[4].Value.ToUpper();
                     string number = match.Groups[1].Value;
 
@@ -120,10 +137,17 @@ namespace BottinToCsvForm.Parsing
                         return number + " " + unit + extra;
                     }
 
+                    // NO UNIT fix
+                    if (match.Groups.Count == 7)
+                    {
+                        string unitMeasure = match.Groups[4].Value;
+                        return unitMeasure;
+                    }
+
                     return number + " " + unit;
                 }
             }
-            return "";
+            return "NO UNIT";
         }
 
         public double ParsePrix(string dataString)
