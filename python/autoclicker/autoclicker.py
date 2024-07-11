152b67630923dc3ce57c7f60783cc287e9757b4e
Lukas Alutalu <121912040+lalutalu@users.noreply.github.com>
2024-07-11 14:19:23 -0700

    get latest file update

--- python/autoclicker/autoclicker.py
+++ python/autoclicker/autoclicker.py
@@ -16,7 +16,7 @@ latest_file = None
 # Function to find the latest downloaded csv file
 def get_latest_file():
     downloads_path = os.path.join(os.path.expanduser('~'), 'Downloads')
-    list_of_files = glob.glob(os.path.join(downloads_path, '*.csv'))
+    list_of_files = glob.glob(os.path.join(downloads_path, '*.xls'))
     if not list_of_files:
         raise ValueError(f"Pas de fichiers CSV dans {downloads_path}")
     latest_file =max(list_of_files, key=os.path.getctime) 
@@ -28,4 +28,4 @@ def get_latest_file():
 
 # MAIN
 if __name__ == "__main__":
-    get_latest_file()
+    print(get_latest_file())
