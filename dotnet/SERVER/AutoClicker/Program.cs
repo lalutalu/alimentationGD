b3e29ffa2eeb49faf718f16391a11eec0c6e6731
Lukas Alutalu <alutalu.lukas@outlook.com>
2024-08-01 15:16:48 -0400

    addition of misc files and new dotnet 3.1 projects

---
+++ dotnet/SERVER/AutoClicker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace GetCoordinates
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
---
+++ dotnet/SERVER/GetCoordinates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace GetCoordinates
+{
+    class Program
+    {
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int
+ X;
+            public int Y;
+        }
+
+        static void
+ Main(string[] args)
+        {
+            Console.WriteLine("Move cursor to desired location within 5 seconds... ");
+            Thread.Sleep(5000);
+
+            POINT cursorPosition;
+            if (GetCursorPos(out cursorPosition))
+            {
+                Console.WriteLine($"Current mouse position: {cursorPosition.X}, {cursorPosition.Y}");
+            }
+            else
+            {
+                Console.WriteLine("Failed to get cursor position.");
+            }
+        }
+    }
+
+}
