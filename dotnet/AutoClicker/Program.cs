3e6a241cf81152cf105185a10861cc5e62cb1fc9
Lukas Alutalu <alutalu.lukas@outlook.com>
2024-08-01 10:11:30 -0700

    project directory

---
+++ dotnet/AutoClicker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoClicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
