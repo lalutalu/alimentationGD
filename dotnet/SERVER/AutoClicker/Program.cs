73c6c60086e91b7296afdbe10ee238cb0000a822
Lukas Alutalu <alutalu.lukas@outlook.com>
2024-08-02 13:28:50 -0400

    lol

--- dotnet/SERVER/AutoClicker/Program.cs
+++ dotnet/SERVER/AutoClicker/Program.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Drawing;
-using System.Threading;
-using System.Runtime.InteropServices;
+﻿using NPOI.HSSF.Extractor;
+using System;
+using System.IO;
 
 namespace GetCoordinates
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            string filePath = "C:\\Users\\lalutalu\\dev\\alimentationGD\\files\\fichier tous les produits.xls";
+            try
+            {
+                using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    var extractor = new OldExcelExtractor(fs);
+                    string text = extractor.Text;
 
+                    //string processedText = Regex.Replace(text, @"\r?\n", " ");
+                    //processedText = processedText.Trim();
+                    //Console.WriteLine(processedText);
+                    Console.WriteLine(text);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading file: " + ex.Message);
+            }
+        }
+
+        public string SeperateStringIntoProductStrings(string initialString)
+        {
+            return "";
         }
     }
-
 }
